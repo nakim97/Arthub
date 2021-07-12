81a537101e1867533230f70ee19816d400950899
elefe <eleferrets@gmail.com>
2021-07-12 19:01:36 -0400

    Reformatted code

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,38 +1,28 @@
-import "./Navbar.css"
-import SubNavbar from "../SubNavbar/SubNavbar"
-import BrushIcon from '@material-ui/icons/Brush';
+import "./Navbar.css";
+import SubNavbar from "../SubNavbar/SubNavbar";
+import BrushIcon from "@material-ui/icons/Brush";
 
-
-export default function Navbar(){
-    return(
+export default function Navbar() {
+  return (
     <div>
-       <nav className="Navbar">
-           <div className="content">
-                <div className="logo">
-                    <BrushIcon style={{ fontSize: 40 }}/>
-                 </div>
+      <nav className="Navbar">
+        <div className="content">
+          <div className="logo">
+            <BrushIcon style={{ fontSize: 40 }} />
+          </div>
 
-                 <div className="search-bar">
-                    <input
-                    type="text"
-                    name="search"
-                    placeholder="Search..."
-                    />
-                </div>
-                <ul className="links">
-                    <li className="navRegister">
-                        Sign Up
-                    </li>
-                    <li className="navLogin">
-                        Sign In
-                    </li>
-                </ul>
-           </div>
-
-       </nav>
-       <div>
-            <SubNavbar />
+          <div className="search-bar">
+            <input type="text" name="search" placeholder="Search..." />
+          </div>
+          <ul className="links">
+            <li className="navRegister">Sign Up</li>
+            <li className="navLogin">Sign In</li>
+          </ul>
         </div>
-       /</div>
-    )
+      </nav>
+      <div>
+        <SubNavbar />
+      </div>
+    </div>
+  );
 }
