2670d2c697558f3945302396529c79bb13b06689
elefe <eleferrets@gmail.com>
2021-07-12 17:14:16 -0400

    Added a browser router to use links

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Home from '../Home/Home'
+import { BrowserRouter } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Home />
+      </BrowserRouter>
     </div>
   );
 }
