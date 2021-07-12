b593afe80658b536886a923d833c4bf907d823cc
elefe <eleferrets@gmail.com>
2021-07-12 15:01:17 -0400

    Added tests and started making them perform well with existing code.

---
+++ backend/utils/tokens.test.js
@@ -0,0 +1,43 @@
+require("regenerator-runtime/runtime");
+test("Something", async () => {
+  expect(0).toEqual(0)
+  })
+// const jwt = require("jsonwebtoken")
+// const tokens = require("./tokens")
+// require("regenerator-runtime/runtime");
+// const { SECRET_KEY } = require("../config")
+
+// describe("Can create valid tokens for users", () => {
+//   test("Valid tokens are created for non-admin users", () => {
+//     const token = tokens.createUserJwt({ username: "test", isAdmin: false })
+//     const payload = jwt.verify(token, SECRET_KEY)
+//     expect(payload).toEqual({
+//       iat: expect.any(Number),
+//       exp: expect.any(Number),
+//       username: "test",
+//       isAdmin: false,
+//     })
+//   })
+
+//   test("Valid tokens are created for admin users", () => {
+//     const token = tokens.createUserJwt({ username: "test", isAdmin: true })
+//     const payload = jwt.verify(token, SECRET_KEY)
+//     expect(payload).toEqual({
+//       iat: expect.any(Number),
+//       exp: expect.any(Number),
+//       username: "test",
+//       isAdmin: true,
+//     })
+//   })
+
+//   test("Tokens set the isAdmin value to false by default", () => {
+//     const token = tokens.createUserJwt({ username: "test" })
+//     const payload = jwt.verify(token, SECRET_KEY)
+//     expect(payload).toEqual({
+//       iat: expect.any(Number),
+//       exp: expect.any(Number),
+//       username: "test",
+//       isAdmin: false,
+//     })
+//   })
+// })
