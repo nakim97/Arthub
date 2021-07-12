89303f4132be2b04439523e43fa3270aeafc302f
elefe <eleferrets@gmail.com>
2021-07-12 12:50:19 -0400

    Added app.js, and the lean server file. Added a test for app

--- backend/app.js
+++ backend/app.js
@@ -1,41 +1,46 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const { NotFoundError } = require("./utils/errors");
+const authRoutes = require("./routes/auth");
+const exerciseRoutes = require("./routes/exercises");
+const sleepRoutes = require("./routes/sleeps");
+const nutritionRoutes = require("./routes/nutritions");
+const activityRoutes = require("./routes/activities");
+const security = require("./middleware/security");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
+// enable cross-origin resource sharing for all origins for all requests
+// NOTE: in production, we'll want to restrict this to only the origin
+// hosting our frontend.
+app.use(cors());
+// parse incoming requests with JSON payloads
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// log requests info
+app.use(morgan("tiny"));
+// for every request, check if a token exists
+// in the authorization header
+// if it does, attach the decoded user to res.locals
+app.use(security.extractUserFromJwt);
+// Use routes after middleware 
+app.use("/auth", authRoutes);
+app.use("/exercise", exerciseRoutes);
+app.use("/sleep", sleepRoutes);
+app.use("/nutrition", nutritionRoutes);
+app.use("/activity", activityRoutes);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+/** Handle 404 errors -- this matches everything */
+app.use((req, res, next) => {
+  return next(new NotFoundError());
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+/** Generic error handler; anything unhandled goes here. */
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message;
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  return res.status(status).json({
+    error: { message, status },
+  });
 });
-
-module.exports = app;
