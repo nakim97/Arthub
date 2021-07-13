0eab36fed5269b0d57b94fc48d449e1986d6778d
elefe <eleferrets@gmail.com>
2021-07-13 18:14:15 -0400

    Removing space

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -16,7 +16,6 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/about" element={<About />} />
           <Route path="/learning" element={<Learning />} />
-       
         </Routes>
       </BrowserRouter>
     </div>
