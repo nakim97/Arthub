8a44e5dd5b6f719d164a95203ec7b17b914ac0f6
elefe <eleferrets@gmail.com>
2021-07-13 15:25:29 -0400

    Actually added the lines element

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -20,14 +20,14 @@ export default function Login({ user, setUser }) {
           <BubbleChartIcon style={{ marginRight: "5px" }} />
         </div>
         <p className="aside">You’re One of Us Now</p>
-        <img className="lines" src={coollines} alt="cool lines img" />
+        <img className="loginImage" src={login1} alt="login main img" /> 
       </div>
       <div className="card">
         <h2>Sign in To ArtHub</h2>
+
         {errors.form && <span className="error">{errors.form}</span>}
         <br />
-
-        <img className="loginImage" src={login1} alt="login main img" />
+        <img className="lines" src={coollines} alt="cool lines img" />
         <div className="form">
           <div className="input-field">
             <label htmlFor="email">Email</label>
