f0e57ef09fe05a211271e08624262964f89d8c34
Rodrigo54099 <rodrigomartinez1477@gmail.com>
2021-07-13 13:05:57 -0700

    updated

---
+++ frontend/src/components/About/About.js
@@ -0,0 +1,22 @@
+import "./About.css";
+import banner2 from "../../Assets/banner2.jpg";
+import Navbar from "../Navbar/Navbar";
+
+export default function About() {
+  return (
+    <div className="about">
+      <Navbar />
+
+      <div className="banner">
+        <img
+          className="bannerImg"
+          src={banner2}
+          alt="people standing on a mountain"
+        />
+        <div className="bannerText">
+          <h4> TOGETHER WE GROW</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
