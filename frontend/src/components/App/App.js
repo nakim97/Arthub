ad8f34dc57ef03788e0fe846a4a4e007a234a610
elefe <eleferrets@gmail.com>
2021-07-14 18:19:38 -0400

    Added all the imports and changed the login/register props

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -8,19 +8,39 @@ import About from "../About/About";
 import Learning from "../Learning/Learning";
 import Market from "../Market/Market";
 import Community from "../Community/Community";
+import apiClient from "../../services/apiClient";
 
 export default function App() {
   const [isFetching, setFetching] = useState(false);
   const [error, setError] = useState(null);
   const [filterInputValue, setInputValue] = useState(null);
   const [user, setUser] = useState({});
+
+  // Fetch users using the jwt token and save or retrieve from local storage
+  useEffect(() => {
+    const fetchUser = async () => {
+      const { data, error } = await apiClient.fetchUserFromToken();
+      if (data) setUser(data.user);
+      if (error) setError(error);
+    };
+    const token = localStorage.getItem("rate_my_setup_token");
+    if (token) {
+      apiClient.setToken(token);
+      fetchUser();
+    }
+  }, []);
+  const handleOnLogout = async () => {
+    await apiClient.logoutUser();
+    setUser({});
+    setError(null);
+  };
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login user={user} setUser={setUser} />} />
+          <Route path="/register" element={<Register user={user} setUser={setUser} />} />
           <Route path="/about" element={<About />} />
           <Route path="/learning" element={<Learning />} />
           <Route path="/market" element={<Market />} />
