a3abd012f238e7f0104caca95b12cf7873b701b4
Rodrigo54099 <rodrigomartinez1477@gmail.com>
2021-07-14 10:55:51 -0700

    initital outline for about page

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -1,5 +1,8 @@
 import "./About.css";
 import banner2 from "../../Assets/banner2.jpg";
+import team1 from "../../Assets/team1.jpeg";
+import team2 from "../../Assets/team2.jpeg";
+import team3 from "../../Assets/team3.jpeg";
 import Navbar from "../Navbar/Navbar";
 
 export default function About() {
@@ -18,17 +21,46 @@ export default function About() {
         </div>
       </div>
 
-      <div className="team">
-        <img className="pictures" src={team1} alt="team member picture 1" />
-        <img className="pictures" src={team2} alt="team member picture 2" />
-        <img className="pictures" src={team3} alt="team member picture 3" />
+      <h2>Meet The Team</h2>
+
+      <div className="aboutImageContainer">
+        <img className="bannerImg" src={team1} alt="team member 1 image" />
+        <img className="bannerImg" src={team2} alt="team member 2 image" />
+        <img className="bannerImg" src={team3} alt="team member 3 image" />
       </div>
 
-      <div className="teamInfo">
-        <p className="memberName">
-          Member Name
-          <span className="description">Small Description for team member</span>
+      <div className="aboutInfo">
+        <p className="aboutName" style={{ textAlign: "left" }}>
+          {" "}
+          Brian Balthazar
         </p>
+        <p className="aboutName" style={{ textAlign: "left" }}>
+          {" "}
+          Stella Kim
+        </p>
+        <p className="aboutName" style={{ textAlign: "left" }}>
+          {" "}
+          Rodrigo Martinez
+        </p>
+      </div>
+
+      <h2>Our Mission</h2>
+
+      <div className="mission">
+        <div className="text">
+          <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum."
+          </p>
+        </div>
+        <div className="missionImgContainer">
+          <img className="missionImg" src={team1} alt="An image of the team" />
+        </div>
       </div>
     </div>
   );
