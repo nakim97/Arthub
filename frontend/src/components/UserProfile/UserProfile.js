4f5c6b104cbc20723cc20158f9d4cd9123cd8a28
Rodrigo54099 <rodrigomartinez1477@gmail.com>
2021-07-15 15:16:28 -0700

    updated

---
+++ frontend/src/components/UserProfile/UserProfile.js
@@ -0,0 +1,10 @@
+import "./UserProfile.css";
+import Navbar from "../Navbar/Navbar";
+
+export default function Explore() {
+  return (
+    <div className="User">
+      <Navbar />
+    </div>
+  );
+}
