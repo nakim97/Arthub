b07ab2175a1d1055133036d96c3c08af07bf27ae
Rodrigo54099 <rodrigomartinez1477@gmail.com>
2021-07-16 08:36:13 -0700

    init

--- frontend/src/components/UserProfile/UserProfile.js
+++ frontend/src/components/UserProfile/UserProfile.js
@@ -1,10 +1,19 @@
 import "./UserProfile.css";
 import Navbar from "../Navbar/Navbar";
+import banner5 from "../../Assets/banner5.png";
 
 export default function Explore() {
   return (
-    <div className="User">
+    <div className="user">
       <Navbar />
+
+      <div className="banner">
+        <img
+          className="bannerImg"
+          src={banner5}
+          alt="people standing on a mountain"
+        />
+      </div>
     </div>
   );
 }
