979a3032605a90b83de305ab048f6ca306750214
elefe <eleferrets@gmail.com>
2021-07-16 14:26:52 -0400

    Added upload preset and cloud name

--- frontend/src/components/ImageUpload/ImageUpload.js
+++ frontend/src/components/ImageUpload/ImageUpload.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import './ImageUpload.css';
-import { useState } from 'react'
+import "./ImageUpload.css";
+import { useState } from "react";
 
 export default function ImageUpload() {
   const [imageUrl, setImageUrl] = useState(null);
@@ -10,57 +9,64 @@ export default function ImageUpload() {
     // create the widget
     window.Cloudinary.createUploadWidget(
       {
-        cloudName: 'john',
-        uploadPreset: 'qv5rfbwg',
+        cloudName: "powerstriders",
+        uploadPreset: "ll7bhjxz",
       },
       (error, result) => {
-        setImageUrl(result.info.secure_url)
-        setImageAlt(`An image of ${result.info.original_filename}`)
-      },
+        setImageUrl(result.info.secure_url);
+        setImageAlt(`An image of ${result.info.original_filename}`);
+      }
     ).open(); // open up the widget after creation
   };
   /*
   This function queries the document to get the first input element with the type of file,
-then it de-structures the files array from the resulting object, 
+then it de-structures the files array from the resulting object,
 then finally logs the first element of the array in the result to the console. */
   const handleImageUpload = () => {
-    const { files } = document.querySelector('input[type="file"]')
-const formData = new FormData();
-formData.append('file', files[0]);
-// replace this with your upload preset name
-formData.append('upload_preset', 'qv5rfbwg');
-const options = {
-  method: 'POST',
-  body: formData,
-};
+    const { files } = document.querySelector('input[type="file"]');
+    const formData = new FormData();
+    formData.append("file", files[0]);
+    // replace this with your upload preset name
+    formData.append("upload_preset", "ll7bhjxz");
+    const options = {
+      method: "POST",
+      body: formData,
+    };
 
-// replace cloudname with your Cloudinary cloud_name
-return fetch('https://api.Cloudinary.com/v1_1/:cloud_name/image/upload', options)
-  .then(res => res.json())
-  .then(res => {
-      setImageUrl(res.secure_url)
-      setImageAlt(`An image of ${res.original_filename}`)
-  })
-  .catch(err => console.log(err));
-  }
-    return (
-      <main className="ImageUpload">
-        <section className="left-side">
-          <form>
-            <div className="form-group">
-              <input type="file"/>
-            </div>
+    // replace cloudname with your Cloudinary cloud_name
+    return fetch(
+      "https://api.Cloudinary.com/v1_1/:powerstriders/image/upload",
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setImageUrl(res.secure_url);
+        setImageAlt(`An image of ${res.original_filename}`);
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <main className="ImageUpload">
+      <section className="left-side">
+        <form>
+          <div className="form-group">
+            <input type="file" />
+          </div>
 
-            <button type="button" className="btn" onClick={handleImageUpload}>Submit</button>
-            <button type="button" className="btn widget-btn" onClick={openWidget}>Upload Via Widget</button>
-          </form>
-        </section>
-        <section className="right-side">
-          <p>The resulting image will be displayed here</p>
-          {imageUrl && (
-            <img src={imageUrl} alt={imageAlt} className="displayed-image"/>
-          )}
-        </section>
-      </main>
-    );
-  }
+          <button type="button" className="btn" onClick={handleImageUpload}>
+            Submit
+          </button>
+          <button type="button" className="btn widget-btn" onClick={openWidget}>
+            Upload Via Widget
+          </button>
+        </form>
+      </section>
+      <section className="right-side">
+        <p>The resulting image will be displayed here</p>
+        {imageUrl && (
+          <img src={imageUrl} alt={imageAlt} className="displayed-image" />
+        )}
+      </section>
+    </main>
+  );
+}
