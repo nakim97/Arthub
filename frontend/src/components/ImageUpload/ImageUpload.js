1f906406c6eb66d73147572cf06c71d2303af7f8
elefe <eleferrets@gmail.com>
2021-07-16 14:44:09 -0400

    Added image upload component

--- frontend/src/components/ImageUpload/ImageUpload.js
+++ frontend/src/components/ImageUpload/ImageUpload.js
@@ -7,7 +7,7 @@ export default function ImageUpload() {
   // We will create the widget and open it up when clicked.
   const openWidget = () => {
     // create the widget
-    window.Cloudinary.createUploadWidget(
+    window.cloudinary.createUploadWidget(
       {
         cloudName: "powerstriders",
         uploadPreset: "ll7bhjxz",
@@ -35,7 +35,7 @@ then finally logs the first element of the array in the result to the console. *
 
     // replace cloudname with your Cloudinary cloud_name
     return fetch(
-      "https://api.Cloudinary.com/v1_1/:powerstriders/image/upload",
+      "https://api.Cloudinary.com/v1_1/powerstriders/image/upload",
       options
     )
       .then((res) => res.json())
@@ -61,7 +61,7 @@ then finally logs the first element of the array in the result to the console. *
           </button>
         </form>
       </section>
-      <section className="right-side">
+      <section className="right-side" style={{color: 'white'}}>
         <p>The resulting image will be displayed here</p>
         {imageUrl && (
           <img src={imageUrl} alt={imageAlt} className="displayed-image" />
@@ -70,3 +70,81 @@ then finally logs the first element of the array in the result to the console. *
     </main>
   );
 }
+
+
+// import React from 'react';
+// import './ImageUpload.css';
+
+// class ImageUpload extends React.Component{
+//   state = {
+//     imageUrl: undefined,
+//     imageAlt: undefined,
+//   }
+
+//   handleImageUpload = () => {
+//     const { files } = document.querySelector('input[type="file"]')
+
+//     const formData = new FormData();
+//     formData.append('file', files[0]);
+//     formData.append('upload_preset', 'qvfwjbwg');
+
+//     const options = {
+//       method: 'POST',
+//       body: formData,
+//     };
+  
+//     return fetch('https://api.cloudinary.com/v1_1/nedy123/image/upload', options)
+//       .then(res => res.json())
+//       .then(res => {
+//         this.setState({
+//           imageUrl: res.secure_url,
+//           imageAlt: `An image of ${res.original_filename}`
+//         })
+//       })
+//       .catch(err => console.log(err));
+//   }
+
+//   openWidget = () => {
+//     window.cloudinary.createUploadWidget(
+//       {
+//         cloudName: 'nedy123',
+//         uploadPreset: 'qvfwjbwg',
+//       },
+//       (error, { event, info }) => {
+//         if (event === 'success') {
+//           this.setState({
+//             imageUrl: info.secure_url,
+//             imageAlt: `An image of ${info.original_filename}`
+//           })
+//         }
+//       },
+//     ).open();
+//   };
+
+//   render() {
+//     const { imageUrl, imageAlt } = this.state;
+
+//     return (
+//       <main  className="App">
+//         <section className="left-side">
+//           <form>
+//             <div className="form-group">
+//               <input type="file"/>
+//             </div>
+  
+//             <button type="button" className="btn" onClick={this.handleImageUpload}>Submit</button>
+//             <button type="button" className="btn widget-btn" onClick={this.openWidget}>Upload Via Widget</button>
+//           </form>
+//         </section>
+//         <section className="right-side">
+//           <p>The resulting image will be displayed here</p>
+//           {imageUrl && (
+//             <img src={imageUrl} alt={imageAlt} className="displayed-image"/>
+//           )}
+//         </section>
+//       </main>
+//     );
+//   }
+// }
+
+// export default ImageUpload;
