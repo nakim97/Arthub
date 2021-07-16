04281da23540f00d334feb090ae282c9d19b0f88
elefe <eleferrets@gmail.com>
2021-07-16 14:00:18 -0400

    Updated class react component to functional component

--- frontend/src/components/ImageUpload/ImageUpload.js
+++ frontend/src/components/ImageUpload/ImageUpload.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import './ImageUpload.css';
+import { useState } from 'react'
 
-class ImageUpload extends React.Component{
-  state = {
-    imageUrl: null,
-    imageAlt: null,
+export default function ImageUpload() {
+  const [imageUrl, setImageUrl] = useState(null);
+  const [imageAlt, setImageAlt] = useState(null);
+  /*
+  This function queries the document to get the first input element with the type of file,
+then it de-structures the files array from the resulting object, 
+then finally logs the first element of the array in the result to the console. */
+  const handleImageUpload = () => {
+    const { files } = document.querySelector('input[type="file"]')
+const formData = new FormData();
+formData.append('file', files[0]);
+// replace this with your upload preset name
+formData.append('upload_preset', 'qv5rfbwg');
+const options = {
+  method: 'POST',
+  body: formData,
+};
+
+// replace cloudname with your Cloudinary cloud_name
+return fetch('https://api.Cloudinary.com/v1_1/:cloud_name/image/upload', options)
+  .then(res => res.json())
+  .then(res => {
+      setImageUrl(res.secure_url)
+      setImageAlt(`An image of ${res.original_filename}`)
+  })
+  .catch(err => console.log(err));
   }
-
-  render() {
-    const { imageUrl, imageAlt } = this.state;
-
     return (
       <main className="ImageUpload">
         <section className="left-side">
@@ -31,6 +50,3 @@ class ImageUpload extends React.Component{
       </main>
     );
   }
-}
-
-export default ImageUpload;
