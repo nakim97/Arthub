4789a8b225739904c71b510b179a452e3fa7a0ea
elefe <eleferrets@gmail.com>
2021-07-16 14:05:13 -0400

    Added widget button

--- frontend/src/components/ImageUpload/ImageUpload.js
+++ frontend/src/components/ImageUpload/ImageUpload.js
@@ -52,7 +52,7 @@ return fetch('https://api.Cloudinary.com/v1_1/:cloud_name/image/upload', options
             </div>
 
             <button type="button" className="btn" onClick={this.handleImageUpload}>Submit</button>
-            <button type="button" className="btn widget-btn">Upload Via Widget</button>
+            <button type="button" className="btn widget-btn" onClick={this.openWidget}>Upload Via Widget</button>
           </form>
         </section>
         <section className="right-side">
