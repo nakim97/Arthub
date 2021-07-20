64ec250e9b18f1736517f2b017208db8b1d94144
elefe <eleferrets@gmail.com>
2021-07-20 14:28:39 -0400

    Added an endpoint to get the posts and the user for the profile page

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const User = require("../models/user");
+const Post = require("../models/post");
 const security = require("../middleware/security");
 const router = express.Router();
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+// /* GET users listing. */
+// router.get("/", function (req, res, next) {
+//   res.send("respond with a resource");
+// });
+router.get("/", security.requireAuthenticatedUser, async (req, res, next) => {
+  try {
+    const { user } = res.locals;
+    const post = await Post.listPhotoPostsForUser({ user });
+    return res.status(200).json({ user, post });
+  } catch (err) {
+    next(err);
+  }
 });
 
 
