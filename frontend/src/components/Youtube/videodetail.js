fd7e96fd3632afe4bcc935a9accb9f61e82d158f
Stella Kim <62909702+nakim97@users.noreply.github.com>
2021-07-28 14:23:10 -0700

    finish modal box & styling

--- frontend/src/components/Youtube/videodetail.js
+++ frontend/src/components/Youtube/videodetail.js
@@ -6,7 +6,7 @@ const VideoDetail = ({ video }) => {
   
 
   const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
-  console.log(typeof video);
+  
   return (
     <div>
       <div className="ui embed" >
--- frontend/src/components/Youtube/youtube.js
+++ frontend/src/components/Youtube/youtube.js
@@ -1,43 +1,94 @@
-import React, { useState } from "react";
+import React from "react";
 import SearchBar from "../Youtube/searchbar";
 import youtube from "../../APIs/youtube";
 import VideoList from "../Youtube/videolist";
 import VideoDetail from "../Youtube/videodetail";
-import OutsideClickHandler from 'react-outside-click-handler';
+import ReactModal from 'react-modal';
+
+
 import "./youtube.css";
 
-export default function Youtube() {
-  const [videos, setVideos] = useState([]);
-  const [selectedVideo, setSelectedVideo] = useState(null);
-  const [hidden, setHidden] = useState(false);
+class YouTube extends React.Component {
   
-  const handleSubmit = async (termFromSearchBar) => {
+  constructor () {
+    super();
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+    
+   
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleVideoSelect = this.handleVideoSelect.bind(this);
+  }
+  
 
+  handleSubmit = async (termFromSearchBar) => {
     const response = await youtube.get("/search", {
       params: {
         q: termFromSearchBar,
       },
     });
-    setVideos(response.data.items);
+
+    this.setState({
+      videos: response.data.items,
+    });
     console.log("this is resp", response);
   };
-  const handleVideoSelect = (video) => {
-    setSelectedVideo(video);
+  handleVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
   };
 
-  return (
-    <div className="ui container" style={{ marginTop: "1em" }}>
-      <SearchBar handleFormSubmit={handleSubmit} />
-      <div className="ui grid">
-        <div className="ui row">
-          <div className="eleven wide column" style={{ marginTop: "800px" }}>
-            {selectedVideo && <VideoDetail video={selectedVideo} />}
-          </div>
-          <div className="five wide column">
-            <VideoList handleVideoSelect={handleVideoSelect} videos={videos} />
+  handleCloseModal () {
+    this.setState({ selectedVideo: null});
+  }
+
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "1em" }}>
+        <SearchBar handleFormSubmit={this.handleSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+           
+            <div className="eleven wide column" style={{marginTop:"800px"}}>
+
+            <ReactModal isOpen={!!this.state.selectedVideo }  
+            onRequestClose={ this.handleCloseModal } 
+            ariaHideApp={false} 
+            style={{
+              overlay: {
+                background:"black",
+                opacity:"0.9"
+              },
+              content: {
+                background:"black",
+                position: 'absolute'
+              }
+            }}
+            >
+                <button className="xbtn" onClick={this.handleCloseModal}> X </button>
+
+                {this.state.selectedVideo &&
+                  <VideoDetail video={this.state.selectedVideo} /> }
+
+            </ReactModal>
+            </div>
+            
+           
+          
+            <div className="five wide column"  >
+                <VideoList 
+                handleVideoSelect={this.handleVideoSelect}
+                videos={this.state.videos} 
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default YouTube;
