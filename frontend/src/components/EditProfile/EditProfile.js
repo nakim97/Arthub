2d415655a511c5469b4276ab8b4ca1318b1e7de2
Rodrigo54099 <rodrigomartinez1477@gmail.com>
2021-08-02 11:57:08 -0700

    updated responsiveness for EditProfile

--- frontend/src/components/EditProfile/EditProfile.js
+++ frontend/src/components/EditProfile/EditProfile.js
@@ -39,127 +39,127 @@ export default function EditProfile({ user, setUser, handleOnLogout }) {
             />
           </div>
         </div>
+      </div>
 
-        <div className="nameInfo">
-          <div className="nameInfoTitle">
-            <h2>Edit Name/Username</h2>
-          </div>
-          <div className="nameUsername">
-            <div className="name">
-              <div className="N1">
-                <label htmlFor="name">Name:</label>
-              </div>
-              <div className="N2">
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Jane Doe"
-                  value={form.name}
-                  onChange={handleOnInputChange}
-                />
-                {errors.name && <span className="error">{errors.name}</span>}
-              </div>
+      <div className="nameInfo">
+        <div className="nameInfoTitle">
+          <h2>Edit Name/Username</h2>
+        </div>
+        <div className="nameUsername">
+          <div className="name">
+            <div className="N1">
+              <label htmlFor="name">Name:</label>
             </div>
-            <div className="username">
-              <div className="U2">
-                <label htmlFor="username">Username:</label>
-              </div>
-              <div className="U2">
-                <input
-                  type="text"
-                  name="userName"
-                  placeholder="Jane Doe"
-                  value={form.userName}
-                  onChange={handleOnInputChange}
-                />
-                {errors.userName && (
-                  <span className="error">{errors.userName}</span>
-                )}
-              </div>
+            <div className="N2">
+              <input
+                type="text"
+                name="name"
+                placeholder="Jane Doe"
+                value={form.name}
+                onChange={handleOnInputChange}
+              />
+              {errors.name && <span className="error">{errors.name}</span>}
             </div>
           </div>
-
-          <div className="bio">
-            <div className="bioTitle">
-              <h2>Edit Bibliography</h2>
+          <div className="username">
+            <div className="U2">
+              <label htmlFor="username">Username:</label>
             </div>
-            <div className="bioDescription">
-              <div className="description">
-                <label htmlFor="bio">Bibliography:</label>
-              </div>
-
-              <div className="textSpace">
-                <input
-                  type="text"
-                  name="biography"
-                  placeholder="Your Biography"
-                  value={form.biography}
-                  onChange={handleOnInputChange}
-                />
-                {errors.biography && (
-                  <span className="error">{errors.biography}</span>
-                )}
-              </div>
+            <div className="U2">
+              <input
+                type="text"
+                name="userName"
+                placeholder="Jane Doe"
+                value={form.userName}
+                onChange={handleOnInputChange}
+              />
+              {errors.userName && (
+                <span className="error">{errors.userName}</span>
+              )}
             </div>
           </div>
         </div>
+      </div>
 
-        <div className="socialMediaEdit">
-          <div className="media">
-            <div className="socialMediaTitle">
-              <h2>Social Media Handles</h2>
+      <div className="bio">
+        <div className="bioTitle">
+          <h2>Edit Bibliography</h2>
+        </div>
+        <div className="bioDescription">
+          <div className="description">
+            <label htmlFor="bio">Bibliography:</label>
+          </div>
+
+          <div className="textSpace">
+            <input
+              type="text"
+              name="biography"
+              placeholder="Your Biography"
+              value={form.biography}
+              onChange={handleOnInputChange}
+            />
+            {errors.biography && (
+              <span className="error">{errors.biography}</span>
+            )}
+          </div>
+        </div>
+      </div>
+
+      <div className="socialMediaEdit">
+        <div className="media">
+          <div className="socialMediaTitle">
+            <h2>Social Media Handles</h2>
+          </div>
+          <div className="IFT">
+            <div className="Instagram">
+              <div className="icon">
+                <InstagramIcon />
+              </div>
+              <div className="iconLink">
+                <input
+                  type="text"
+                  name="instagramUrl"
+                  placeholder="Your Instagram url"
+                  value={form.instagramUrl}
+                  onChange={handleOnInputChange}
+                />
+                {errors.instagramUrl && (
+                  <span className="error">{errors.instagramUrl}</span>
+                )}
+              </div>
             </div>
-            <div className="IFT">
-              <div className="Instagram">
-                <div className="icon">
-                  <InstagramIcon />
-                </div>
-                <div className="iconLink">
-                  <input
-                    type="text"
-                    name="instagramUrl"
-                    placeholder="Your Instagram url"
-                    value={form.instagramUrl}
-                    onChange={handleOnInputChange}
-                  />
-                  {errors.instagramUrl && (
-                    <span className="error">{errors.instagramUrl}</span>
-                  )}
-                </div>
+            <div className="Facebook">
+              <div className="icon">
+                <FacebookIcon />
               </div>
-              <div className="Facebook">
-                <div className="icon">
-                  <FacebookIcon />
-                </div>
-                <div className="iconLink">
-                  <input
-                    type="text"
-                    name="facebookUrl"
-                    placeholder="Your Facebook url"
-                    value={form.facebookUrl}
-                    onChange={handleOnInputChange}
-                  />
-                  {errors.facebookUrl && (
-                    <span className="error">{errors.facebookUrl}</span>
-                  )}
-                </div>
+              <div className="iconLink">
+                <input
+                  type="text"
+                  name="facebookUrl"
+                  placeholder="Your Facebook url"
+                  value={form.facebookUrl}
+                  onChange={handleOnInputChange}
+                />
+                {errors.facebookUrl && (
+                  <span className="error">{errors.facebookUrl}</span>
+                )}
               </div>
-              <div className="Twitter">
-                <div className="icon">
-                  <TwitterIcon />
-                </div>
-                <div className="iconLink">
-                  <input
-                    type="text"
-                    name="twitterUrl"
-                    placeholder="Your Twitter url"
-                    value={form.twitterUrl}
-                    onChange={handleOnInputChange}
-                  />
-                  {errors.twitterUrl && (
-                    <span className="error">{errors.twitterUrl}</span>
-                  )}
-                </div>
+            </div>
+            <div className="Twitter">
+              <div className="icon">
+                <TwitterIcon />
+              </div>
+              <div className="iconLink">
+                <input
+                  type="text"
+                  name="twitterUrl"
+                  placeholder="Your Twitter url"
+                  value={form.twitterUrl}
+                  onChange={handleOnInputChange}
+                />
+                {errors.twitterUrl && (
+                  <span className="error">{errors.twitterUrl}</span>
+                )}
               </div>
             </div>
           </div>
