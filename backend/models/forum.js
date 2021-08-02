d22028a8a3a0b5f78bde81a65c3b5cde3bc4242a
Brian Balthazar <eleferrets@gmail.com>
2021-08-02 09:33:07 -0400

    Added starting files for forums

---
+++ backend/models/forum.js
@@ -0,0 +1,96 @@
+const { BadRequestError } = require("../utils/errors");
+const db = require("../db");
+
+class Post {
+  static async listAllPosts() {
+    const results = await db.query(
+      `
+      SELECT pp.id AS "photoPostId",
+    pp.post_title AS "postTitle",
+    pp.post_description AS "postDescription",
+    img.id AS "imgId",
+    img.post_img_url AS "imgPostUrl"
+    FROM photoPost AS pp
+    JOIN photoUpload AS img ON img.id = pp.img_id
+    `
+    );
+    return results.rows;
+  }
+  static async listPhotoPostsForUser({ user }) {
+    const results = await db.query(
+      `
+    SELECT pp.id AS "photoPostId",
+    pp.post_title AS "postTitle",
+    pp.post_description AS "postDescription",
+    img.id AS "imgId",
+    img.post_img_url AS "imgPostUrl",
+    u.email AS "userEmail"
+    FROM photoPost AS pp
+    JOIN users AS u ON u.id = pp.user_id
+    JOIN photoUpload AS img ON img.id = pp.img_id
+    WHERE u.id = (SELECT id FROM users WHERE email = $1)
+    `,
+      [user.email]
+    );
+    return results.rows;
+  }
+  static async fetchPhotoPostById(postId) {
+    if (!postId) {
+      throw new BadRequestError("No id provided");
+    }
+
+    const query = `SELECT * FROM photoPost AS pp
+    JOIN users AS u ON u.id = pp.user_id
+    JOIN photoUpload AS img ON img.id = pp.img_id
+    WHERE pp.id = $1`;
+    const result = await db.query(query, [postId]);
+    const post = result.rows[0];
+    return post;
+  }
+  static async deletePhotoPostById(postId) {
+    if (!postId) {
+      throw new BadRequestError("No id provided");
+    }
+    const query = `DELETE FROM photoPost WHERE id = $1`;
+    const result = await db.query(query, [postId]);
+    const user = "ok";
+    return user;
+  }
+
+  static async searchByTitle(query) {
+    if (!query) {
+      throw new BadRequestError("No search query provided");
+    }
+    const dbquery = `Select * from photoPost where post_title like '%${query}%';
+    `;
+    console.log(dbquery);
+    const result = await db.query(dbquery);
+    const queries = result.rows;
+    return queries;
+  }
+
+  static async createPost({ post, user }) {
+    if (!post || !Object.keys(post).length) {
+      throw new BadRequestError("No post info provided");
+    }
+    if (!user) {
+      throw new BadRequestError("No user provided");
+    }
+    const results = await db.query(
+      `
+            INSERT INTO photoPost (post_title, post_description, img_id, user_id)
+            VALUES ($1, $2, $3, (SELECT id FROM users WHERE email = $4))
+            RETURNING id,
+            user_id AS "userId",
+            img_id,
+            post_title,
+            post_description,
+            photo_created_at
+            `,
+      [post.postTitle, post.postDescription, post.imgId, user.email]
+    );
+    return results.rows[0];
+  }
+}
+
+module.exports = Post;
---
+++ backend/routes/forums.js
@@ -0,0 +1,155 @@
+const express = require("express");
+const Post = require("../models/post");
+const Comment = require("../models/picComments");
+const Like = require("../models/picLikes");
+const security = require("../middleware/security");
+const router = express.Router();
+
+router.post("/", security.requireAuthenticatedUser, async (req, res, next) => {
+  try {
+    const { user } = res.locals;
+    const post = await Post.createPost({
+      user,
+      post: req.body,
+    });
+    return res.status(201).json({ post });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/listPosts", async (req, res, next) => {
+  try {
+    const posts = await Post.listAllPosts();
+    return res.status(200).json({ posts });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/", security.requireAuthenticatedUser, async (req, res, next) => {
+  try {
+    // List all posts for user
+    const { user } = res.locals;
+    const postsByMe = await Post.listPhotoPostsForUser({ user });
+    return res.status(200).json({ postsByMe });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/search", async (req, res, next) => {
+  try {
+    //uses search?q= -str- - you enter the string
+    const { q } = req.query;
+    const searches = await Post.searchByTitle(q);
+    return res.status(200).json({ searches });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// fetch single post
+router.get("/:postsId", async (req, res, next) => {
+  try {
+    const postsId = req.params.postsId;
+    const posting = await Post.fetchPhotoPostById(postsId);
+    // console.log("pID", postsId, "p", posts)
+    res.status(200).json({ posting });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// delete single post
+router.delete("/:postsId", async (req, res, next) => {
+  try {
+    const postsId = req.params.postsId;
+    const posting = await Post.deletePhotoPostById(postsId);
+    // console.log("pID", postsId, "p", posts)
+    res.status(200).json({ posting });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Get the comments for the photo post
+router.get("/:postsId/comments", async (req, res, next) => {
+  try {
+    const comments = await Comment.fetchCommentForPostByUser({
+      postsId: req.params.postsId,
+    });
+
+    return res.status(201).json({ comments });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Create a new comment
+router.post(
+  "/:postsId/comments",
+  security.requireAuthenticatedUser,
+  async (req, res, next) => {
+    try {
+      const { user } = res.locals;
+      const comment = await Comment.postComment({
+        comment_description: req.body.comment,
+        user,
+        post_id: req.params.postsId,
+      });
+      return res.status(201).json({ comment });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+// Get the likes for a post
+router.get("/:postsId/likes", async (req, res, next) => {
+  try {
+    const likes = await Like.fetchLikesForPost({
+      postsId: req.params.postsId,
+    });
+    return res.status(201).json({ likes });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Create the likes for a post
+router.post(
+  "/:postsId/likes",
+  security.requireAuthenticatedUser,
+  async (req, res, next) => {
+    try {
+      const { user } = res.locals;
+      const like = await Like.postLike({
+        user,
+        post_id: req.params.postsId,
+      });
+      return res.status(201).json({ like });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.delete(
+  "/:postsId/likes",
+  security.requireAuthenticatedUser,
+  async (req, res, next) => {
+    try {
+      const { user } = res.locals;
+      const liked = await Like.deleteLike({
+        user,
+        post_id: req.params.postsId,
+      });
+      return res.status(201).json({ liked });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+module.exports = router;
