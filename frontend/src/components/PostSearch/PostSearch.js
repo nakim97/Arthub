cacb1faf00bd29558163f84d56eee32d58db6c3b
Brian Balthazar <eleferrets@gmail.com>
2021-08-03 11:30:20 -0400

    Updated components

---
+++ frontend/src/components/PostSearch/PostSearch.js
@@ -0,0 +1,57 @@
+import "./PostSearch.css";
+import Navbar from "../Navbar/Navbar";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { Link } from "react-router-dom";
+import { usePostSearch } from "../../hooks/usePostSearch";
+
+export default function PostSearch({ user, handleOnLogout }) {
+  const { posts } = usePostSearch({ user });
+
+  //Unauthenticated view
+  if (!user.email) {
+    return (
+      <div className="total">
+        <Navbar user={user} handleOnLogout={handleOnLogout} />
+        <div className="title">
+          <h2>You must be logged in to view your portfolio posts.</h2>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="PostSearch">
+      <Navbar user={user} handleOnLogout={handleOnLogout} />
+      <div className="content">
+        <h3 className="title">Edit Your Portfolio</h3>
+      </div>
+      {posts.map((post) => (
+        <div className="portfolioposts" key={post.photoPostId}>
+          <div className="portfolioImg">
+            <Link to={`/post/${post.photoPostId}`}>
+              <img
+                className="portfolioImg"
+                src={`${post.imgPostUrl}`}
+                alt={`Post ${post.photoPostId}`}
+              />
+            </Link>
+          </div>
+          <div className="post" style={{ position: "relative" }}>
+            <h4 className="portfolioTitle"> {post.postTitle}</h4>
+            <p className="portfolioDescription"> {post.postDescription}</p>
+            <p className="likeComments">
+              {" "}
+              {/* <EditIcon style={{ fontSize: "20px" }} />{" "} */}
+              {/* <button
+                className="clear"
+                onClick={() => handleDelete(post.photoPostId)}
+              >
+                <DeleteIcon className="icon" />{" "}
+              </button> */}
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ frontend/src/hooks/usePostSearch.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/apiClient";
+
+export const usePostSearch = ({ user }) => {
+  const [posts, setPosts] = useState([]);
+  const [isFetching, setFetching] = useState(false);
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [errors, setError] = useState({});
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setFetching(true);
+      try {
+        const { data } = await apiClient.listPosts(user);
+
+        setPosts(data.postsByMe);
+      } catch (err) {
+        setError(err);
+      }
+
+      setFetching(false);
+    };
+    fetchPosts();
+  }, [user, posts]);
+
+  // const handleDelete = async (postId) => {
+  //   setIsProcessing(true);
+  //   try {
+  //     const { data } = await apiClient.deletePost(postId);
+  //   } catch (err) {
+  //     setError(err);
+  //   }
+  //   setIsProcessing(false);
+  // };
+
+  return { posts };
+};
