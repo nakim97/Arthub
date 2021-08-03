f7fffd35e0718f9b45f39479842e5b34c2b44d34
Brian Balthazar <eleferrets@gmail.com>
2021-08-03 18:21:06 -0400

    Started adding some things to forum posts

--- frontend/src/components/CreateForumPost/CreateForumPost.js
+++ frontend/src/components/CreateForumPost/CreateForumPost.js
@@ -3,8 +3,41 @@ import CommunityNavbar from "../CommunityNavbar/CommunityNavbar";
 import Navbar from "../Navbar/Navbar";
 import React from "react";
 import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
+import { useUploadForm } from "../../hooks/useUploadForm";
+import ImageUpload from "../ImageUpload/ImageUpload";
+import { useImageUpload } from "../../hooks/useImageUpload";
 
 export default function CreateForumPost({ user, handleOnLogout, term, setTerm }) {
+  const { handleImageUpload, openWidget, imageUrl, imageAlt } =
+    useImageUpload();
+  const {
+    tagOptions,
+    CustomColorCheckbox,
+    checked,
+    checked1,
+    checked2,
+    handleChange,
+    handleChange1,
+    handleChange2,
+    form,
+    setForm,
+    error,
+    handleOnInputChange,
+    handleOnSubmit,
+    isProcessing,
+  } = useUploadForm({ imageUrl, imageAlt });
+   //Unauthenticated view
+   if (!user.email) {
+    return (
+      <div className="total">
+        <Navbar user={user} handleOnLogout={handleOnLogout} term={term} setTerm={setTerm}   />
+        <div className="title">
+          <h2>You must be logged in to create a forum post.</h2>
+        </div>
+      </div>
+    );
+  }
+  // Normal view
   return (
     <div className="CreateForumPost">
       <Navbar user={user} handleOnLogout={handleOnLogout} term={term} setTerm={setTerm} />
@@ -21,7 +54,7 @@ export default function CreateForumPost({ user, handleOnLogout, term, setTerm })
           <div className="description">
             <textarea placeholder="Text(optional)" />
           </div>
-          <div className="tags">
+          {/* <div className="tags">
             <ul>
               <li className="bubbletag"> + General</li>
               <li className="bubbletag"> + Advice</li>
@@ -30,7 +63,7 @@ export default function CreateForumPost({ user, handleOnLogout, term, setTerm })
               <li className="bubbletag"> + Art</li>
               <li className="bubbletag"> + Other</li>
             </ul>
-          </div>
+          </div> */}
 
           <button className="postBtn">POST</button>
         </div>
