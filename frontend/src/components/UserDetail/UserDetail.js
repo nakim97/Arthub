b6f13ab6f61082fecaaa896908cf224970bbac7e
Brian Balthazar <eleferrets@gmail.com>
2021-08-06 12:19:03 -0400

    Took out unneeded code

--- frontend/src/components/UserDetail/UserDetail.js
+++ frontend/src/components/UserDetail/UserDetail.js
@@ -24,7 +24,6 @@ export default function UserDetail({ user, handleOnLogout, term, setTerm }) {
       setIsLoading(true);
       try {
         const { data } = await apiClient.listUserWithId(Id);
-        console.log(data)
         setMyUser(data.using);
       } catch (err) {
         setError(err);
@@ -40,7 +39,6 @@ export default function UserDetail({ user, handleOnLogout, term, setTerm }) {
        setFetching(true);
        try {
          const { data } = await apiClient.listPosts(myUser);
-         console.log(data)
          setPosts(data.postsByMe);
        } catch (err) {
          setError(err);
@@ -49,7 +47,7 @@ export default function UserDetail({ user, handleOnLogout, term, setTerm }) {
        setFetching(false);
      };
      fetchPosts();
-   }, [user, posts]);
+   }, [myUser, posts]);
 
   const renderPostContent = () => {
     if (isLoading) return <h1>Loading...</h1>;
@@ -132,7 +130,6 @@ let instagram_url = "",
   );
     return (
         <div className="user">
-      {/* <Navbar user={user} handleOnLogout={handleOnLogout} term={term} setTerm={setTerm}   /> */}
 
       <div className="banners">{banner_img}</div>
 
@@ -172,17 +169,7 @@ let instagram_url = "",
           <h2>Portfolio</h2>
         </div>
 
-        {/* <div className="editPageButns">
-          <div className="editPortfolio">
-            <Link to="/editportfolio">Edit Portfolio</Link>
-          </div>
-          <div className="editProfile">
-            <Link to="/edit">Edit Profile</Link>
-          </div>
-          <div className="uploadProfile">
-            <Link to="/upload">Create Post</Link>
-          </div>
-        </div> */}
+        
       </div>
 
       <div className="pictureArea">
@@ -212,156 +199,5 @@ let instagram_url = "",
       {renderPostContent()}
     </div>
   );
-  // const { myName, username, posts, userInfo } = useUserProfile({ user });
-  // let instagram_url = "",
-  //   facebook_url = "",
-  //   twitter_url = "";
-  // const instagram =
-  //   userInfo.instagram_url == null || userInfo.instagram_url == "null";
-  // // We have a link to use
-  // if (!instagram) {
-  //   instagram_url = "http://" + userInfo.instagram_url;
-  // }
-  // const facebook =
-  //   userInfo.facebook_url == null || userInfo.facebook_url == "null";
-  // // We have a link to use
-  // if (!facebook) {
-  //   facebook_url = "http://" + userInfo.facebook_url;
-  // }
-  // const twitter =
-  //   userInfo.twitter_url == null || userInfo.twitter_url == "null";
-  // // We have a link to use
-  // if (!twitter) {
-  //   twitter_url = "http://" + userInfo.twitter_url;
-  // }
-  // const banner_url =
-  //   userInfo.banner_img_url == null || userInfo.banner_img_url == "null";
-  // const banner_img = banner_url ? (
-  //   <>
-  //     <img
-  //       className="bannerImg"
-  //       src={userBanner}
-  //       alt="people standing on a mountain"
-  //     />
-  //   </>
-  // ) : (
-  //   <>
-  //     <img
-  //       className="bannerImg"
-  //       src={`${userInfo.banner_img_url}`}
-  //       alt="my banner"
-  //     />
-  //   </>
-  // );
-  // const profile_url =
-  //   userInfo.profile_img_url == null || userInfo.profile_img_url == "null";
-  // const profile_img = profile_url ? (
-  //   <>
-  //     {/* Return default image */}
-  //     <img className="profilePic" src={person2} alt="user profile picture" />
-  //   </>
-  // ) : (
-  //   <>
-  //     {/* Use our own image */}
-  //     <img
-  //       className="profilePic"
-  //       src={`${userInfo.profile_img_url}`}
-  //       alt="my profile"
-  //     />
-  //   </>
-  // );
-  // const bio = userInfo.biography == null || userInfo.biography == "null";
-  // const biography = bio ? (
-  //   <>
-  //     <div className="description">
-  //       <p>No biography</p>
-  //     </div>
-  //   </>
-  // ) : (
-  //   <>
-  //     <div className="description">
-  //       <p>{`${userInfo.biography}`}</p>
-  //     </div>
-  //   </>
-  // );
-  // //Unauthenticated view
-  // // if (!user.email) {
-  // //   return (
-  // //     <div className="total">
-  // //       <Navbar user={user} handleOnLogout={handleOnLogout} term={term} setTerm={setTerm}   />
-  // //       <div className="title">
-  // //         <h2>You must be logged in to view your profile.</h2>
-  // //       </div>
-  // //     </div>
-  // //   );
-  // // }
-  // return (
-  //   <div className="user">
-  //     <Navbar user={user} handleOnLogout={handleOnLogout} term={term} setTerm={setTerm}   />
-
-  //     <div className="banners">{banner_img}</div>
-
-  //     <div className="userInfo">
-  //       <div className="profile">
-  //         <div className="profilePic">{profile_img}</div>
-  //         <div className="profileNameUser">
-  //           <div className="profileName">
-  //             <p>{myName}</p>
-  //           </div>
-  //           <div className="profileUsername">
-  //             <p>{username}</p>
-  //           </div>
-  //         </div>
-
-  //         <div className="mediaLinks">
-  //           <div className="socialMedia">
-  //             <a target="_blank" href={`${instagram_url}`}>
-  //               <InstagramIcon />
-  //             </a>
-  //             <a target="_blank" href={`${facebook_url}`}>
-  //               <FacebookIcon />
-  //             </a>
-  //             <a target="_blank" href={`${twitter_url}`}>
-  //               <TwitterIcon />
-  //             </a>
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </div>
-  //     <div className="userBio">
-  //       <div className="description">{biography}</div>
-  //     </div>
-
-  //     <div className="portfolioArea">
-  //       <div className="Portfolio">
-  //         <h2>Portfolio</h2>
-  //       </div>
-
-  //       {/* <div className="editPageButns">
-  //         <div className="editPortfolio">
-  //           <Link to="/editportfolio">Edit Portfolio</Link>
-  //         </div>
-  //         <div className="editProfile">
-  //           <Link to="/edit">Edit Profile</Link>
-  //         </div>
-  //         <div className="uploadProfile">
-  //           <Link to="/upload">Create Post</Link>
-  //         </div>
-  //       </div> */}
-  //     </div>
-
-  //     <div className="pictureArea">
-  //       {posts.map((post) => (
-  //         <div className="image" key={post.photoPostId}>
-  //           <Link to={`/post/${post.photoPostId}`}>
-  //             <img
-  //               src={`${post.imgPostUrl}`}
-  //               alt={`Portfolio ${post.photoPostId}`}
-  //             ></img>
-  //           </Link>
-  //         </div>
-  //       ))}
-  //     </div>
-  //   </div>
-  // );
+ 
 }
