2ac8fa86ea71ee1f3f2da9712cf9c42953e7baeb
Brian Balthazar <eleferrets@gmail.com>
2021-08-09 17:01:04 -0400

    Refactored userprofile and added noreferrer as a security fix

--- frontend/src/components/UserProfile/UserProfile.js
+++ frontend/src/components/UserProfile/UserProfile.js
@@ -1,91 +1,33 @@
 import "./UserProfile.css";
 import { Link } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
-import userBanner from "../../Assets/userBanner.png";
-import person2 from "../../Assets/person2.png";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import { useUserProfile } from "../../hooks/useUserProfile";
+import { useUserHandles } from "../../hooks/useUserHandles";
 
 export default function UserProfile({ user, handleOnLogout, term, setTerm }) {
   const { myName, username, posts, userInfo } = useUserProfile({ user });
-  let instagram_url = "",
-    facebook_url = "",
-    twitter_url = "";
-  const instagram =
-    userInfo.instagram_url == null || userInfo.instagram_url == "null";
-  // We have a link to use
-  if (!instagram) {
-    instagram_url = "http://" + userInfo.instagram_url;
-  }
-  const facebook =
-    userInfo.facebook_url == null || userInfo.facebook_url == "null";
-  // We have a link to use
-  if (!facebook) {
-    facebook_url = "http://" + userInfo.facebook_url;
-  }
-  const twitter =
-    userInfo.twitter_url == null || userInfo.twitter_url == "null";
-  // We have a link to use
-  if (!twitter) {
-    twitter_url = "http://" + userInfo.twitter_url;
-  }
-  const banner_url =
-    userInfo.banner_img_url == null || userInfo.banner_img_url == "null";
-  const banner_img = banner_url ? (
-    <>
-      <img
-        className="bannerImg"
-        src={userBanner}
-        alt="people standing on a mountain"
-      />
-    </>
-  ) : (
-    <>
-      <img
-        className="bannerImg"
-        src={`${userInfo.banner_img_url}`}
-        alt="my banner"
-      />
-    </>
-  );
-  const profile_url =
-    userInfo.profile_img_url == null || userInfo.profile_img_url == "null";
-  const profile_img = profile_url ? (
-    <>
-      {/* Return default image */}
-      <img className="profilePic" src={person2} alt="user profile picture" />
-    </>
-  ) : (
-    <>
-      {/* Use our own image */}
-      <img
-        className="profilePic"
-        src={`${userInfo.profile_img_url}`}
-        alt="my profile"
-      />
-    </>
-  );
-  const bio = userInfo.biography == null || userInfo.biography == "null";
-  const biography = bio ? (
-    <>
-      <div className="description">
-        <p>No biography</p>
-      </div>
-    </>
-  ) : (
-    <>
-      <div className="description">
-        <p>{`${userInfo.biography}`}</p>
-      </div>
-    </>
-  );
+  const {
+    biography,
+    instagram_url,
+    facebook_url,
+    twitter_url,
+    banner_img,
+    profile_img,
+  } = useUserHandles({ userInfo });
+
   //Unauthenticated view
   if (!user.email) {
     return (
       <div className="total">
-        <Navbar user={user} handleOnLogout={handleOnLogout} term={term} setTerm={setTerm}   />
+        <Navbar
+          user={user}
+          handleOnLogout={handleOnLogout}
+          term={term}
+          setTerm={setTerm}
+        />
         <div className="title">
           <h2>You must be logged in to view your profile.</h2>
         </div>
@@ -94,7 +36,12 @@ export default function UserProfile({ user, handleOnLogout, term, setTerm }) {
   }
   return (
     <div className="user">
-      <Navbar user={user} handleOnLogout={handleOnLogout} term={term} setTerm={setTerm}   />
+      <Navbar
+        user={user}
+        handleOnLogout={handleOnLogout}
+        term={term}
+        setTerm={setTerm}
+      />
 
       <div className="banners">{banner_img}</div>
 
@@ -112,13 +59,13 @@ export default function UserProfile({ user, handleOnLogout, term, setTerm }) {
 
           <div className="mediaLinks">
             <div className="socialMedia">
-              <a target="_blank" href={`${instagram_url}`}>
+              <a target="_blank" rel="noreferrer" href={`${instagram_url}`}>
                 <InstagramIcon />
               </a>
-              <a target="_blank" href={`${facebook_url}`}>
+              <a target="_blank" rel="noreferrer" href={`${facebook_url}`}>
                 <FacebookIcon />
               </a>
-              <a target="_blank" href={`${twitter_url}`}>
+              <a target="_blank" rel="noreferrer" href={`${twitter_url}`}>
                 <TwitterIcon />
               </a>
             </div>
---
+++ frontend/src/hooks/useUserHandles.js
@@ -0,0 +1,85 @@
+import userBanner from "../../Assets/userBanner.png";
+import person2 from "../../Assets/person2.png";
+
+export const useUserHandles = ({ userInfo }) => {
+  let instagram_url = "",
+    facebook_url = "",
+    twitter_url = "";
+  const instagram =
+    userInfo.instagram_url == null || userInfo.instagram_url == "null";
+  // We have a link to use
+  if (!instagram) {
+    instagram_url = "http://" + userInfo.instagram_url;
+  }
+  const facebook =
+    userInfo.facebook_url == null || userInfo.facebook_url == "null";
+  // We have a link to use
+  if (!facebook) {
+    facebook_url = "http://" + userInfo.facebook_url;
+  }
+  const twitter =
+    userInfo.twitter_url == null || userInfo.twitter_url == "null";
+  // We have a link to use
+  if (!twitter) {
+    twitter_url = "http://" + userInfo.twitter_url;
+  }
+  const banner_url =
+    userInfo.banner_img_url == null || userInfo.banner_img_url == "null";
+  const banner_img = banner_url ? (
+    <>
+      <img
+        className="bannerImg"
+        src={userBanner}
+        alt="people standing on a mountain"
+      />
+    </>
+  ) : (
+    <>
+      <img
+        className="bannerImg"
+        src={`${userInfo.banner_img_url}`}
+        alt="my banner"
+      />
+    </>
+  );
+  const profile_url =
+    userInfo.profile_img_url == null || userInfo.profile_img_url == "null";
+  const profile_img = profile_url ? (
+    <>
+      {/* Return default image */}
+      <img className="profilePic" src={person2} alt="user profile" />
+    </>
+  ) : (
+    <>
+      {/* Use our own image */}
+      <img
+        className="profilePic"
+        src={`${userInfo.profile_img_url}`}
+        alt="my profile"
+      />
+    </>
+  );
+  const bio = userInfo.biography == null || userInfo.biography == "null";
+  const biography = bio ? (
+    <>
+      <div className="description">
+        <p>No biography</p>
+      </div>
+    </>
+  ) : (
+    <>
+      <div className="description">
+        <p>{`${userInfo.biography}`}</p>
+      </div>
+    </>
+  );
+
+  return {
+    biography,
+    instagram_url,
+    facebook_url,
+    twitter_url,
+    banner_img,
+    profile_img,
+  };
+};
