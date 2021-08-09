c631f9f664826e17361c7d5a30a34f7f82604260
Rodrigo54099 <rodrigomartinez1477@gmail.com>
2021-08-09 08:36:09 -0700

    small change

--- frontend/src/APIs/youtube.js
+++ frontend/src/APIs/youtube.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const KEY = "AIzaSyA1ZBd213Vjp6zU7GhKx07MLvGQkbfYZjk"; // mention your youtube API key here
+const KEY = "AIzaSyA1ZBd213Vjp6zU7GhKx07MLvGQkbfYZjk"; // mention your youtube API key here!
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3/",
