59f19cc60c6d5400a5248bc7bc3eb64655a7ed4a
Rodrigo54099 <rodrigomartinez1477@gmail.com>
2021-08-09 11:08:36 -0700

    current mess with css styling

--- frontend/src/components/Community/Community.js
+++ frontend/src/components/Community/Community.js
@@ -132,7 +132,7 @@ export default function Community({
 
       <div
         className="bannerBackground"
-        style={{ backgroundImage: `url(${communityHero})`, height:"880px"}}
+        style={{ backgroundImage: `url(${communityHero})`, height: "880px" }}
       >
         {" "}
       </div>
